63187d6798db6ff929a49d031bb82fc51fe64e08
konrad <konrad@mint-VirtualBox.(none)>
2013-02-25 14:51:53 +0100

    add a remove method to fileProperties

--- FileSystem/scanner.py
+++ FileSystem/scanner.py
@@ -11,9 +11,13 @@ class fileProperties:
 		print(self.s)
 		print(self.m)
 
+        def remove(self):
+            print("remove"+self.n)
+
 		
 def scanFiles(path):
 	print("scanning: "+path)
 	fp = fileProperties("name",1200,"a")
 	fp.show()
+        fp.remove()
 	return 0
